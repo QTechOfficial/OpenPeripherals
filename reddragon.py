8a756335de05e954d2cb39c5d78cb1bcf59ad078
Thomas Muller <thomasmuller336@gmail.com>
2020-01-19 20:36:50 -0500

    Started laying out class structure, implimented basic functionality

--- reddragon.py
+++ reddragon.py
@@ -1,25 +1,102 @@
 from pprint import pprint as pp
+from random import randint
 import hid
+import time
+import colorsys
 
 vid = 0x0c45
 pid = 0x5004
 
-with hid.Device(path=b'/dev/hidraw1') as h:
-    print(f'Manufac: {h.manufacturer}')
-    print(f'Prod: {h.product}')
-    print(f'Ser: {h.serial}')
+packet = [
+    '04 01 00 01',
+    '04 11 01 06 03 09 00 00 0000ff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+    '04 37 10 11 36 00 00 00 0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000000000ff0000ff0000ff0000000000',
+    '04 6e 0f 11 36 36 00 00 0000000000000000000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff00ffff0000ff0000',
+    '04 a1 12 11 36 6c 00 00 0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff00ff0000ff0000',
+    '04 d7 12 11 36 a2 00 00 0000ff00ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff00ffff0000ff0000',
+    '04 13 0d 11 36 d8 00 00 0000ff0000ff0000ff0000000000ff0000000000000000000000ff0000ff0000ff0000000000ff0000000000ff0000ffff00ff0000ff0000',
+    '04 47 0f 11 36 0e 01 00 0000ff0000ff0000ff0000ff0000ff0000ff0000000000ff0000000000ff0000000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000',
+    '04 81 0b 11 36 44 01 00 0000ff0000ff0000ff0000ff0000ff0000ff0000000000000000000000000000000000ff0000ff0000ff0000000000ff00ffff0000000000',
+    '04 c2 00 11 36 7a 01 00 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+    '04 02 00 02',
+]
 
-    #print(h.write(b'\x04\x01\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
-    #print(f'data: {h.read(100)}')
 
-    print(h.write(b'\x04\x0a\x00\x06\x02\x00\x00\x00\x10\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
+def phex(data):
+    print(' '.join([hex(d) for d in data]))
 
-    # Brightness 0-5
-    #print(h.write(b'\x04\x0a\x00\x06\x02\x00\x00\x00\x00<BRIGHTNESS>\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
 
-    #print(h.write(b'\x00\xdb\x56\x0e\x9d\x99\xff\xff\x43\x01\x82\x0c\x03\x00\x2d\x00\xed\x8b\x05\x5e\x00\x00\x00\x00\x65\x94\x0a\x00\x00\x00\x00\x00\x40\x00\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x04\x0b\x00\x06\x01\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
+class RedDragon:
+    def __init__(self, hid):
+        self._hid = hid
 
-    #print(h.write(b'\x00\xb8\x61\x3e\x9e\x99\xff\xff\x43\x01\x82\x0c\x03\x00\x2d\x00\xed\x8b\x05\x5e\x00\x00\x00\x00\x63\xe6\x0a\x00\x00\x00\x00\x00\x40\x00\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x04\x02\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
+    def write_packet(self, cmd, data=bytes()):
+        cksum = cmd
 
-#    while True:
-    print(f'data: {h.read(100)}')
+        for dat in data:
+            cksum += dat
+
+        self._hid.write(bytes([4, 1, 0, 1]))  # Start block
+        h.read(256, 10)
+        ledata = bytes([4, cksum & 0xFF, 0, cmd]) + data
+        print(len(ledata))
+        #phex(ledata)
+        self._hid.write(ledata)
+        h.read(256, 10)
+        self._hid.write(bytes([4, 2, 0, 2]))  # End block
+        h.read(256, 10)
+
+    def set_effect(self, val):
+        # TODO: Check argument
+        self.write_packet(6, bytes([1, 0, 0, 0, val]))
+
+    def set_brightness(self, val):
+        # TODO: Check argument
+        self.write_packet(6, bytes([1, 1, 0, 0, val]))
+
+    def set_speed(self, val):
+        # TODO: Check argument
+        self.write_packet(6, bytes([1, 2, 0, 0, val]))
+
+    def set_direction(self, val):
+        # TODO: Check argument
+        self.write_packet(6, bytes([1, 3, 0, 0, val]))
+
+    def set_rainbow_puke(self, val):
+        # TODO: Check argument
+        self.write_packet(6, bytes([1, 4, 0, 0, val]))
+
+    def set_effect_color(self, r, g, b):
+        # TODO: Check argument
+        self.write_packet(6, bytes([3, 5, 0, 0, r, g, b]))
+
+    def set_color_data(self, offset, data):
+        # TODO: Check argument
+        self.write_packet(0x11, bytes([len(data), offset & 0x00FF, offset >> 8 & 0x00FF, 0]) + data)
+
+    def set_some_color(self, r, g, b):
+        self.write_packet(6, bytes([3, 9, 0, 0, r, g, b]))
+
+
+if __name__ == '__main__':
+    with hid.Device(path=b'/dev/hidraw1') as h:
+        print(f'Manufac: {h.manufacturer}')
+        print(f'Prod: {h.product}')
+        print(f'Ser: {h.serial}')
+
+        rd = RedDragon(h)
+
+        rd.set_effect(0x14)
+
+        #rd.set_some_color(255, 0, 0)
+
+        for i in range(0x7D):
+            rd.set_color_data(i * 3, bytes([0, 0, 0]))
+
+        time.sleep(2)
+
+        for i in range(0x7D):
+            rd.set_color_data(0x36*2, bytes([255 - int(i/0x7D*0xFF), 0, int(i/0x7D*0xFF)]*(i+1)))
+            #time.sleep(0.1)
+
+        #rd.set_some_color(255, 0, 0)
