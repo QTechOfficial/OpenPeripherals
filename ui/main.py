f1ed66ac6300f23be9e7f529822d5b90c19b080d
ValsCSGO <valscsgo@gmail.com>
2020-01-20 22:13:03 -0500

    Added color picker for key display

--- ui/main.py
+++ ui/main.py
@@ -4,10 +4,9 @@ import sys
 from functools import partial
 
 from PyQt5.QtCore import QRect, QObject, pyqtSlot
-from PyQt5.QtWidgets import QApplication, QPushButton
+from PyQt5.QtWidgets import QApplication, QPushButton, QColorDialog
 from PyQt5.QtDBus import QDBusConnection, QDBusInterface
 from PyQt5.uic import loadUi
-
 
 class Keyboard(QObject):
     def __init__(self, parent=None):
@@ -17,13 +16,11 @@ class Keyboard(QObject):
         self.ui.show()
 
         self.buttons = {}
+        self.active_color = [0, 0, 0]
+        self.color_dialog = QColorDialog()
 
         self.bus = QDBusConnection.sessionBus()
         self.kb = QDBusInterface('com.qtech.openkeyboard.test', '/com/qtech/openkeyboard/test', 'com.qtech.openkeyboard.test.properties', QDBusConnection.sessionBus())
-
-    @pyqtSlot(str)
-    def print_name(self, name):
-        print(name)
 
     @pyqtSlot(int)
     def on_set_brightness(self, val):
@@ -34,13 +31,29 @@ class Keyboard(QObject):
         print(val)
         self.kb.call('SetEffect', val)
 
+    @pyqtSlot()
+    def open_color_dialog(self):
+        dialog = self.color_dialog.getColor()
+        self.set_active_color(dialog.red(), dialog.green(), dialog.blue())
+        self.set_color(self.ui.set_color)
+
+    def set_active_color(self, r, g, b):
+        self.active_color = [ r, g, b ]
+
+    def set_color(self, button):
+        col = self.active_color
+        color = f'rgb({col[0]}, {col[1]}, {col[2]})'
+        button.setStyleSheet('QPushButton { background-color: ' + color + ' }')
+
     def connect_buttons(self):
         self.ui.set_brightness.valueChanged.connect(self.on_set_brightness)
         self.ui.set_effect.currentIndexChanged.connect(self.on_set_effect)
 
+        self.ui.set_color.clicked.connect(self.open_color_dialog)
+
         for b in self.buttons.keys():
             button = self.buttons[b]
-            button.clicked.connect(partial(self.print_name, b))
+            button.clicked.connect(partial(self.set_color, self.buttons[b]))
             button.show()
 
     def add_buttons(self):
