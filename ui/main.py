91ee5df1191b5446bd3223428627c999401fd084
Thomas Muller <thomasmuller336@gmail.com>
2020-01-21 23:44:26 -0500

    Removed hardcoded theme

--- ui/main.py
+++ ui/main.py
@@ -22,7 +22,7 @@ class Keyboard(QObject):
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
 
-        self.ui = loadUi('./keyboard.ui')
+        self.ui = loadUi('keyboard.ui')
         self.ui.show()
 
         self.buttons = {}
@@ -116,26 +116,9 @@ class Keyboard(QObject):
                 button.setText(label)
                 self.buttons[name] = Key(name, button, led_offset)
 
+
 def main():
     app = QApplication(sys.argv)
-
-    app.setStyle("Fusion")
-
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, QColor(255, 255, 255))
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(255, 255, 255))
-    palette.setColor(QPalette.ToolTipText, QColor(255, 255, 255))
-    palette.setColor(QPalette.Text, QColor(255, 255, 255))
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))
-    palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
-    app.setPalette(palette)
 
     kb = Keyboard()
     kb.add_buttons()
