7abe4c260f04c001d737f8e0137b57aaf4b0b983
ValsCSGO <valscsgo@gmail.com>
2020-01-23 00:31:52 -0500

    Changed window title to OpenPeripherals Changed default key color to white Changed keys to buttons Changed some key names in definition file Moved objects to their own folder Moved set_button_color to utils file Added support to set key colors from a dict

--- ui/main.py
+++ ui/main.py
@@ -10,8 +10,9 @@ from PyQt5.QtDBus import QDBusConnection, QDBusInterface
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.uic import loadUi
 
-from color import Color
-from key import Key
+from utils import set_button_color
+from objects.color import Color
+from objects.key import Key
 
 class Keyboard(QObject):
     DBUS_SERVICE = 'com.qtech.openkeyboard'
@@ -25,9 +26,9 @@ class Keyboard(QObject):
         self.ui = loadUi('keyboard.ui')
         self.ui.show()
 
-        self.buttons = {}
-        self.active_color = Color(0, 0, 0)
-        self.effect_color = Color(0, 0, 0)
+        self.keys = {}
+        self.active_color = Color(255, 255, 255)
+        self.effect_color = Color(255, 255, 255)
         self.color_dialog = QColorDialog()
 
         self.bus = QDBusConnection.sessionBus()
@@ -46,10 +47,10 @@ class Keyboard(QObject):
 
         return devices
 
-    @staticmethod
-    def set_button_color(button, color):
-        col = f'rgb({color.r}, {color.g}, {color.b})'
-        button.setStyleSheet('QPushButton { background-color: ' + col + ' }')
+    def get_all_colors(self, key_colors):
+        for key_id in key_colors.keys():
+            color = key_colors[key_id]
+            self.set_key_color(key_id, Color(color[0], color[1], color[2]))
 
     @pyqtSlot(int)
     def on_set_brightness(self, val):
@@ -60,26 +61,26 @@ class Keyboard(QObject):
         self.kb_leds.call('SetEffect', val)
 
     @pyqtSlot(str, Color)
-    def set_key_color(self, button_id, color):
-        self.kb_leds.call('SetKeyColor', self.buttons[button_id].led_offset, color.r, color.g, color.b)
-        self.set_button_color(self.buttons[button_id].button, color)
+    def set_key_color(self, key_id, color):
+        self.kb_leds.call('SetKeyColor', self.keys[key_id].led_offset, color.r, color.g, color.b)
+        self.keys[key_id].set_color(color)
 
     @pyqtSlot()
     def on_set_all(self):
-        for button_id in self.buttons.keys():
-            self.set_key_color(button_id, self.active_color)
+        for key_id in self.keys.keys():
+            self.set_key_color(key_id, self.active_color)
 
     @pyqtSlot()
     def on_change_primary_color(self):
         col = self.color_dialog.getColor()
         self.active_color.set(col.red(), col.green(), col.blue())
-        self.set_button_color(self.ui.set_color, self.active_color)
+        set_button_color(self.ui.set_color, self.active_color)
 
     @pyqtSlot()
     def on_change_effect_color(self):
         col = self.color_dialog.getColor()
         self.effect_color.set(col.red(), col.green(), col.blue())
-        self.set_button_color(self.ui.set_effect_color, self.effect_color)
+        set_button_color(self.ui.set_effect_color, self.effect_color)
         self.kb_leds.call('SetEffectColor', self.effect_color.r, self.effect_color.g, self.effect_color.b)
 
     def connect_buttons(self):
@@ -90,15 +91,14 @@ class Keyboard(QObject):
         self.ui.set_effect_color.clicked.connect(self.on_change_effect_color)
         self.ui.set_all.clicked.connect(self.on_set_all)
 
-        for button_id in self.buttons.keys():
-            button = self.buttons[button_id].button
-            button.clicked.connect(partial(self.set_key_color, button_id, self.active_color))
+        for key_id in self.keys.keys():
+            button = self.keys[key_id].button
+            button.clicked.connect(partial(self.set_key_color, key_id, self.active_color))
             button.show()
 
     def add_buttons(self):
         with open('reddragon.def', 'r') as keyfile:
             lines = keyfile.readlines()
-
             for line in lines:
                 item = line.split(',')
 
@@ -114,7 +114,7 @@ class Keyboard(QObject):
                 button.setObjectName("key_" + name)
                 button.setGeometry(QRect(x, y, w, h))
                 button.setText(label)
-                self.buttons[name] = Key(name, button, led_offset)
+                self.keys[name] = Key(name, button, led_offset)
 
 
 def main():
