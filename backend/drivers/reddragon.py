bdc931e2bd807ad528438154ca212a24a718c51b
Thomas Muller <thomasmuller336@gmail.com>
2020-01-24 01:19:04 -0500

    Properties, Protocols, Punit tests Finally realised that properties are not special, moved size and offset logic into write_packet where it belongs Added start/end block calls into get/set_property Added getters for all properties Added 'unit tests' Finally added the effect property to notes, dont know why that took so long...

--- backend/drivers/reddragon.py
+++ backend/drivers/reddragon.py
@@ -46,75 +46,96 @@ class Properties:
     DIRECTION = 0x03
     RAINBOW = 0x04
     EFFECT_COLOR = 0x05
+    SOME_COLOR = 0x09
 
 
 class RedDragon:
     def __init__(self, hid):
         self._hid = hid
-        self._brightness = 0
-        self._effect = 0
 
-    def write_packet(self, cmd, data):
+    def write_packet(self, cmd, size=1, offset=0, *data):
         cksum = cmd
 
         for dat in data:
             cksum += dat
 
-        self._hid.write(bytes([4, cksum & 0xFF, 0, cmd] + data))
-
-        return self._hid.read(128, 10)
+        # Packet structure: [Header(4), Checksum, Padding?, Command, Size, OffsetL, OffsetH, Padding(0), ...]
+        self._hid.write(bytes([4, cksum & 0xFF, 0, cmd, size, offset & 0x00FF, offset >> 8 & 0x00FF, 0] + list(data)))
+        resp = self._hid.read(128, 10)
+        # Hidapi seems to be bad at freeing things or something because a bunch of garbage gets tacked on to the
+        # end of the responses. I just cut out the parts that are valid based on the size argument.
+        resp_len = resp[4]
+        return tuple(resp[8:8+resp_len])
 
     def set_property(self, prop_id, *values):
-        self.write_packet(Commands.SET_PROPERTY, [1, prop_id, 0, 0] + values)
+        self.write_packet(Commands.START_BLOCK)
+        self.write_packet(Commands.SET_PROPERTY, len(values), prop_id, *values)
+        self.write_packet(Commands.END_BLOCK)
 
-    def set_effect(self, val):
+    def get_property(self, prop_id, size=1):
+        self.write_packet(Commands.START_BLOCK)
+        resp = self.write_packet(Commands.GET_PROPERTY, size, prop_id)
+        self.write_packet(Commands.END_BLOCK)
+        return resp
+
+    def set_effect(self, effect):
         # TODO: Check argument
-        self.write_packet(Commands.SET_PROPERTY, bytes([1, 0, 0, 0, val]))
-        self._effect = val
+        self.set_property(Properties.EFFECT, effect)
 
     def get_effect(self):
-        # TODO: Actually get from keyboard
-        return self._effect
+        return self.get_property(Properties.EFFECT)[0]
 
-    def set_brightness(self, val):
+    def set_brightness(self, brightness):
         # TODO: Check argument
-        self.write_packet(6, bytes([1, 1, 0, 0, val]))
-        self._brightness = val
+        self.set_property(Properties.BRIGHTNESS, brightness)
 
     def get_brightness(self):
-        # TODO: Actually get from keyboard
-        return self._brightness
+        return self.get_property(Properties.BRIGHTNESS)[0]
 
-    def set_speed(self, val):
+    def set_speed(self, speed):
         # TODO: Check argument
-        self.write_packet(6, bytes([1, 2, 0, 0, val]))
+        self.set_property(Properties.SPEED, speed)
 
-    def set_direction(self, val):
-        # TODO: Check argument
-        self.write_packet(6, bytes([1, 3, 0, 0, val]))
+    def get_speed(self):
+        return self.get_property(Properties.SPEED)[0]
 
-    def set_rainbow_puke(self, val):
+    def set_direction(self, direction):
         # TODO: Check argument
-        self.write_packet(6, bytes([1, 4, 0, 0, val]))
+        self.set_property(Properties.DIRECTION, direction)
+
+    def get_direction(self):
+        return self.get_property(Properties.DIRECTION)[0] == 1
+
+    def set_rainbow(self, rainbow):
+        # TODO: Check argument
+        self.set_property(Properties.RAINBOW, rainbow)
+
+    def get_rainbow(self):
+        return self.get_property(Properties.RAINBOW)[0] == 1
 
     def set_effect_color(self, r, g, b):
         # TODO: Check argument
-        self.write_packet(6, bytes([3, 5, 0, 0, r, g, b]))
+        self.set_property(Properties.EFFECT_COLOR, r, g, b)
+
+    def get_effect_color(self):
+        return self.get_property(Properties.EFFECT_COLOR, 3)
 
     def set_color_data(self, offset, data):
         # TODO: Check argument
-        self.write_packet(0x11, bytes([len(data), offset & 0x00FF, offset >> 8 & 0x00FF, 0]) + data)
+        self.write_packet(Commands.SET_KEY_COLORS, len(data), offset, *data)
 
     def get_color_data(self, size=0x36, offset=0x00):
         # TODO: Check argument
-        resp = self.write_packet(0x10, bytes([size, offset & 0x00FF, offset >> 8 & 0x00FF, 0]))
-        # Hidapi seems to be bad at freeing things or something because a bunch of garbage gets tacked on to the
-        # end of the responses. I just cut out the parts that are valid based on the size argument.
-        colors_raw = resp[8:8+size]
-        return colors_raw
+        resp = self.write_packet(Commands.GET_KEY_COLORS, size, offset)
+        return resp
 
+    # TODO: Find out what this actually does
     def set_some_color(self, r, g, b):
-        self.write_packet(6, bytes([3, 9, 0, 0, r, g, b]))
+        # TODO: Check to see if the 3 is important, if not, switch to set_property
+        self.set_property(Properties.SOME_COLOR, r, g, b)
+
+    def get_some_color(self):
+        return self.get_property(Properties.SOME_COLOR, 3)
 
     def get_all_colors(self):
         colors = {}
@@ -128,6 +149,7 @@ class RedDragon:
         # keys += ['ESC', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', '<BLANK>', 'PRTSC', 'SCLK', 'PAUSE']
         colors = []
 
+        self.write_packet(Commands.START_BLOCK)
         colors_raw = self.get_color_data(0x36, 0x00)
         colors_raw += self.get_color_data(0x36, 0x36)
         colors_raw += self.get_color_data(0x36, 0x6C)
@@ -135,6 +157,7 @@ class RedDragon:
         colors_raw += self.get_color_data(0x36, 0xD8)
         colors_raw += self.get_color_data(0x36, 0x10E)
         colors_raw += self.get_color_data(0x36, 0x144)
+        self.write_packet(Commands.END_BLOCK)
         phex(colors_raw)
 
         for i in range(0, len(colors_raw), 3):
@@ -152,17 +175,57 @@ if __name__ == '__main__':
 
         rd = RedDragon(h)
 
+        # TODO: If only there was some kind of testing framework I could use instead of this shit... Hmmm
+        print('Effects')
+        for i in range(15):
+            rd.set_effect(i)
+            print(i, rd.get_effect() == i)
+            time.sleep(0.1)
+
+        print('Brightness')
+        for i in range(6):
+            rd.set_brightness(i)
+            print(i, rd.get_brightness() == i)
+            time.sleep(0.1)
+
+        print('Speed')
+        for i in range(6):
+            rd.set_speed(i)
+            print(i, rd.get_speed() == i)
+            time.sleep(0.1)
+
+        print('Direction')
+        rd.set_direction(True)
+        time.sleep(0.1)
+        print(rd.get_direction() == True)
+        rd.set_direction(False)
+        time.sleep(0.1)
+        print(rd.get_direction() == False)
+
+        print('Rainbow')
+        rd.set_rainbow(True)
+        time.sleep(0.1)
+        print(rd.get_rainbow() == True)
+        rd.set_rainbow(False)
+        time.sleep(0.1)
+        print(rd.get_rainbow() == False)
+
+        print('Effect Color')
+        rd.set_effect_color(255, 0, 0)
+        time.sleep(0.1)
+        print(rd.get_effect_color() == (255, 0, 0))
+        rd.set_effect_color(0, 255, 0)
+        time.sleep(0.1)
+        print(rd.get_effect_color() == (0, 255, 0))
+        rd.set_effect_color(0, 0, 255)
+        time.sleep(0.1)
+        print(rd.get_effect_color() == (0, 0, 255))
+
+        print('Color Data')
         rd.set_effect(0x14)
+        rd.set_color_data(0, [255, 0, 0, 0, 255, 0, 0, 0, 255])
+        print(rd.get_color_data(9, 0) == (255, 0, 0, 0, 255, 0, 0, 0, 255))
 
-        #rd.set_some_color(255, 0, 0)
-
-        for i in range(0x7D):
-            rd.set_color_data(i * 3, bytes([255, 0, 0]))
-
-        time.sleep(2)
-
-        #for i in range(0x7D):
-        #    rd.set_color_data(0x36*2, bytes([255 - int(i/0x7D*0xFF), 0, int(i/0x7D*0xFF)]*(i+1)))
-            #time.sleep(0.1)
-
-        #rd.set_some_color(255, 0, 0)
+        print('Some Color')
+        rd.set_some_color(255, 0, 0)
+        print(rd.get_some_color() == (255, 0, 0))
