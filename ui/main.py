8866b03141579347605d76729ec341aff7a1b096
Thomas Muller <thomasmuller336@gmail.com>
2020-01-25 01:59:47 -0500

    Merge branch 'ui' into rewrite

--- ui/main.py
+++ ui/main.py
@@ -9,8 +9,9 @@ from PyQt5.QtWidgets import QApplication, QPushButton, QColorDialog
 from PyQt5.QtDBus import QDBusConnection, QDBusInterface
 from PyQt5.uic import loadUi
 
-from color import Color
-from key import Key
+from utils import set_button_color
+from objects.color import Color
+from objects.key import Key
 
 class Keyboard(QObject):
     DBUS_SERVICE = 'com.qtech.openkeyboard'
@@ -24,9 +25,9 @@ class Keyboard(QObject):
         self.ui = loadUi('keyboard.ui')
         self.ui.show()
 
-        self.buttons = {}
-        self.active_color = Color(0, 0, 0)
-        self.effect_color = Color(0, 0, 0)
+        self.keys = {}
+        self.active_color = Color(255, 255, 255)
+        self.effect_color = Color(255, 255, 255)
         self.color_dialog = QColorDialog()
 
         self.bus = QDBusConnection.sessionBus()
@@ -58,12 +59,12 @@ class Keyboard(QObject):
         self.ui.set_effect.setCurrentIndex(effect)
         self.ui.set_brightness.setValue(brightness)
         self.effect_color = Color(*effect_color)
-        self.set_button_color(self.ui.set_effect_color, Color(*effect_color))
+        set_button_color(self.ui.set_effect_color, Color(*effect_color))
 
-    @staticmethod
-    def set_button_color(button, color):
-        col = f'rgb({color.r}, {color.g}, {color.b})'
-        button.setStyleSheet('QPushButton { background-color: ' + col + ' }')
+    def get_all_colors(self, key_colors):
+        for key_id in key_colors.keys():
+            color = key_colors[key_id]
+            self.on_set_key_color(key_id, Color(color[0], color[1], color[2]))
 
     @pyqtSlot(int)
     def on_set_brightness(self, val):
@@ -74,26 +75,33 @@ class Keyboard(QObject):
         self.kb_leds.call('SetEffect', val)
 
     @pyqtSlot(str, Color)
-    def set_key_color(self, button_id, color):
-        self.kb_leds.call('SetKeyColor', self.buttons[button_id].led_offset, color.r, color.g, color.b)
-        self.set_button_color(self.buttons[button_id].button, color)
+    def on_set_key_color(self, key_id, color):
+        self.kb_leds.call('SetKeyColor', self.keys[key_id].led_offset, color.r, color.g, color.b)
+        self.keys[key_id].set_color(color)
 
     @pyqtSlot()
     def on_set_all(self):
-        for button_id in self.buttons.keys():
-            self.set_key_color(button_id, self.active_color)
+        for key_id in self.keys.keys():
+            self.on_set_key_color(key_id, self.active_color)
+
+    def on_get_all(self):
+        keys = {}
+        for key_id in self.keys.keys():
+            color = self.keys[key_id].color
+            keys[key_id] = (color.r, color.g, color.b)
+        return keys
 
     @pyqtSlot()
     def on_change_primary_color(self):
         col = self.color_dialog.getColor()
         self.active_color.set(col.red(), col.green(), col.blue())
-        self.set_button_color(self.ui.set_color, self.active_color)
+        set_button_color(self.ui.set_color, self.active_color)
 
     @pyqtSlot()
     def on_change_effect_color(self):
         col = self.color_dialog.getColor()
         self.effect_color.set(col.red(), col.green(), col.blue())
-        self.set_button_color(self.ui.set_effect_color, self.effect_color)
+        set_button_color(self.ui.set_effect_color, self.effect_color)
         self.kb_leds.call('SetEffectColor', self.effect_color.r, self.effect_color.g, self.effect_color.b)
 
     def connect_buttons(self):
@@ -104,15 +112,14 @@ class Keyboard(QObject):
         self.ui.set_effect_color.clicked.connect(self.on_change_effect_color)
         self.ui.set_all.clicked.connect(self.on_set_all)
 
-        for button_id in self.buttons.keys():
-            button = self.buttons[button_id].button
-            button.clicked.connect(partial(self.set_key_color, button_id, self.active_color))
+        for key_id in self.keys.keys():
+            button = self.keys[key_id].button
+            button.clicked.connect(partial(self.on_set_key_color, key_id, self.active_color))
             button.show()
 
     def add_buttons(self):
         with open('reddragon.def', 'r') as keyfile:
             lines = keyfile.readlines()
-
             for line in lines:
                 item = line.split(',')
 
@@ -128,7 +135,7 @@ class Keyboard(QObject):
                 button.setObjectName("key_" + name)
                 button.setGeometry(QRect(x, y, w, h))
                 button.setText(label)
-                self.buttons[name] = Key(name, button, led_offset)
+                self.keys[name] = Key(name, button, led_offset)
 
 
 if __name__ == "__main__":
